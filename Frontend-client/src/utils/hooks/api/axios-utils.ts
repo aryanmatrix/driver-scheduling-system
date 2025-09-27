41166170bd6f6f7e5b339f245ee471387bd66e14
aryanmatrix <pradeepar374@gmail.com>
2025-09-27 07:30:41 +0300

    fixed uploading images on the server

--- Frontend-client/src/utils/hooks/api/axios-utils.ts
+++ Frontend-client/src/utils/hooks/api/axios-utils.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base configuration
 const axiosInstance = axios.create({
-    // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001",
+    // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:6060",
     baseURL: "https://driver-scheduling-system-5kmi.vercel.app",
     headers: {
         "Content-Type": "application/json",
